953e9f3166d35ce0be6576d07890d5f101728e1b
Alejandro Parra <alejandro_parra_15@hotmail.com>
2021-04-01 13:34:44 -0400

    feat(Detail): This for NAME acoording to dynamic link

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import Home from './src/screens/Home';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
+import ThisIsForName from './src/screens/ThisIsForName';
+import {getNameFromUrl} from './src/helpers/getNameFromUrl';
 
 const App = () => {
+  const [isReffered, setIsReffered] = useState(null);
+  useEffect(() => {
+    dynamicLinks()
+      .getInitialLink()
+      .then(link => {
+        const name = getNameFromUrl(link.url);
+        setIsReffered(name);
+      })
+      .catch(() => console.log('Without shareable link'));
+  }, []);
+  const resetIsReffered = () => {
+    setIsReffered(null);
+  };
   return (
     <>
       <SafeAreaView style={{backgroundColor: '#ffffff'}}>
-        <Home />
+        {!isReffered ? (
+          <Home />
+        ) : (
+          <ThisIsForName
+            name={isReffered}
+            handleScreenOpenend={resetIsReffered}
+          />
+        )}
       </SafeAreaView>
     </>
   );
