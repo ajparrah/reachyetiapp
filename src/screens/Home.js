5fae03ade6d21ff2d0676607cd1a0619ea8d7fdf
Alejandro Parra <alejandro_parra_15@hotmail.com>
2021-04-01 11:14:29 -0400

    feat(Project): React native firebase setup

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <>
+      <View>
+        <Text style={{color: '#000000', fontSize: 40}}>Esta es una prueba</Text>
+      </View>
+    </>
+  );
+};
+
+export default Home;
